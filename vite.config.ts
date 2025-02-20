f8317c20e97b9818d8666ba0b945aa0856f28dc4
bowman-crego <bowmanccrego@gmail.com>
2025-02-20 10:10:58 -0800

    added allowed hosts to viteconfig to attempt to allow permissions to those who log on to the site

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
   envDir: "./env",
   plugins: [react()],
   server: {
+    host: true,
     port: 3001,
     open: true,
   },
+  preview: {
+    allowedHosts: ["https://candidate-search-0my2.onrender.com"]
+  }
+  
 });
